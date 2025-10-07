4440a6d6f53b78e806541ab1ba9c031b1b614e38
24520579 <24520579@gm.uit.edu.vn>
2025-10-07 16:34:05 +0700

    Bai01 BTH2

---
+++ BTH2_TangChanHong_24520579/Bai01/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write(" - Month: ");
+        int month = int.Parse(Console.ReadLine());
+        Console.Write(" - Year: ");
+        int year = int.Parse(Console.ReadLine());
+
+        DateTime firstDay = new DateTime(year, month, 1);
+        int startDay = (int)firstDay.DayOfWeek;  
+        int daysInMonth = DateTime.DaysInMonth(year, month);
+
+        Console.Clear();
+        Console.WriteLine($"Month: {month:D2}/{year}");
+        Console.WriteLine("SUN  MON  TUE  WED  THU  FRI  SAT");
+
+
+        for (int i = 0; i < startDay; i++)
+            Console.Write("     ");
+
+        for (int day = 1; day <= daysInMonth; day++)
+        {
+            Console.Write($"{day,2}   ");
+            if ((day + startDay) % 7 == 0)
+                Console.WriteLine();
+        }
+
+        Console.WriteLine();
+    }
+}
