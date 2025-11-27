cdf284b478f28ebfb4ae018c75630d568c0e0657
24520579 <24520579@gm.uit.edu.vn>
2025-11-27 21:24:42 +0700

    Add files via upload

---
+++ BTH4_TangChanHong_24520579/Bai01/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Bai01
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void clickShow(object sender, MouseEventArgs e)
+        {
+            MessageBox.Show(e.Button.ToString() + "\nTọa độ chuột "+e.X+":"+e.Y);
+        }
+
+        private void keyShow(object sender, KeyEventArgs e)
+        {
+            MessageBox.Show("Phím: " + e.KeyCode.ToString() + "\nMã ASCII: "+e.KeyValue);
+        }
+    }
+}
